5f0da056214c968d99d5ca60885fd250c1e279c4
verny <verny.mata@icumed.com>
2021-11-25 19:45:18 -0600

    Cambios en entidad y service

--- Entity/UsuarioEntity.cs
+++ Entity/UsuarioEntity.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Entity;
 
 namespace Entity
 {
     public class UsuarioEntity : DBEntity
     {
+        public string? Usuario { get; set; }
+        public string Nombre { get; set; }
+        public string Clave { get; set; }
+        public int Rol_id { get; set; }
+        public string Rol_Description { get; }
+        public Boolean Estado { get; set; }
     }
 }
--- WBL/UsuarioService.cs
+++ WBL/UsuarioService.cs
@@ -1,27 +1,87 @@
-﻿using Entity;
+﻿using DB;
+using Entity;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+
 
 namespace WBL
 {
     public interface IUsuarioService
     {
-        Task<IEnumerable<ProductoEntity>> Get();
-
+        Task<DBEntity> Login(UsuarioEntity entity);
+        Task<DBEntity> Create(UsuarioEntity entity);
+        Task<DBEntity> Update(UsuarioEntity entity);
+        Task<DBEntity> Delete(UsuarioEntity entity);
+        Task<IEnumerable<UsuarioEntity>> Get();
         Task<ProductoEntity> GetById(UsuarioEntity entity);
+
+        Task<IEnumerable<UsuarioEntity >> GetLista();
     }
 
     public class UsuarioService : IUsuarioService
     {
-        public Task<IEnumerable<ProductoEntity>> Get()
+        private readonly IDataAccess sql;
+
+        public UsuarioService(IDataAccess _sql)
+        {
+            sql = _sql;
+        }
+
+        #region MetodosCrud
+        public async Task<DBEntity> Create(UsuarioEntity entity)
+        {
+            try
+            {
+                var result = sql.ExecuteAsync("UsuarioInsertar", new
+                {
+                    entity.Usuario,
+                    entity.Nombre,
+                    entity.Clave,
+                    entity.Estado,
+                    entity.Rol_id
+                });
+
+                return await result;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        Task<DBEntity> IUsuarioService.Delete(UsuarioEntity entity)
         {
             throw new NotImplementedException();
         }
 
-        public Task<ProductoEntity> GetById(UsuarioEntity entity)
+        Task<IEnumerable<UsuarioEntity>> IUsuarioService.Get()
         {
             throw new NotImplementedException();
         }
+
+        Task<ProductoEntity> IUsuarioService.GetById(UsuarioEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<IEnumerable<UsuarioEntity>> IUsuarioService.GetLista()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<DBEntity> IUsuarioService.Login(UsuarioEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<DBEntity> IUsuarioService.Update(UsuarioEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
     }
 }
