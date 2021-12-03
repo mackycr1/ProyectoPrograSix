c836912a130e62841ffe30ecf19b3fd7b0b975bb
mackycr1 <mackycr1@gmail.com>
2021-12-02 20:04:13 -0600

    Updated Cliente.CSHTML file with code to edit customer.

--- Programacion61/Pages/Cliente/Edit.cshtml.cs
+++ Programacion61/Pages/Cliente/Edit.cshtml.cs
@@ -9,8 +9,62 @@ namespace Programacion61.Pages.Cliente
 {
     public class EditModel : PageModel
     {
-        public void OnGet()
+        private readonly IClienteService clienteService;
+
+        public EditModel (IClienteService clienteService)
+	    {
+            this.clienteService = clienteService;
+	    }
+
+        //Enables us to call the customer entity attributes
+        [BindProperty]
+        [FromBody]
+        public ClienteEntity Entity { get; set; } = new ClienteEntity();
+
+        //Maps the Id to be edited
+        [BindProperty(SupportsGet =true)]
+        public int? id { get; set; }
+
+        public async Task<IActionResult> OnGet()
         {
+            try
+            {
+                //Edit
+                if (id.HasValue)
+                {
+                    Entity = await clienteService.GetById(new() { IdCliente = id });
+                }
+
+                return Page();
+            }
+            catch (Exception ex)
+            {
+                return new JsonResult(new DBEntity { CodeError = ex.HResult, MsgError = ex.Message });
+            }
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            try
+            {
+                var result = new DBEntity();
+
+                //Edit
+                if (Entity.IdCliente.HasValue)
+                {
+                    result = await clienteService.Update(Entity);
+                }
+                else
+                {
+                    result = await clienteService.Insert(Entity);
+                }
+
+                return new JsonResult(result);
+            }
+            catch (Exception ex)
+            {
+                return new JsonResult(new DBEntity { CodeError = ex.HResult, MsgError = ex.Message });
+            }
         }
     }
 }
