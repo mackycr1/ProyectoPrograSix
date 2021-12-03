3d36b94b6deca02810c61775ce1e72d7d807ced3
mackycr1 <mackycr1@gmail.com>
2021-12-02 20:08:24 -0600

    Created Edit Type Script for customer

--- Programacion61/wwwroot/App/Cliente/Edit.js
+++ Programacion61/wwwroot/App/Cliente/Edit.js
@@ -1,2 +1,37 @@
 "use strict";
+var ClienteEdit;
+(function (ClienteEdit) {
+    var Entity = $("#AppEdit").data('entity');
+    var Formulario = new Vue({
+        data: {
+            Formulario: "#FormEdit",
+            Entity: Entity
+        },
+        methods: {
+            Save: function () {
+                if (BValidateData(this.Formulario)) {
+                    Loading.fire("Guardando");
+                    App.AxiosProvider.ClienteGuardar(this.Entity).then(function (data) {
+                        Loading.close();
+                        /*If no error exist*/
+                        if (data.CodeError == 0) {
+                            /*Message indicating the file was successfully deleted*/
+                            Toast.fire({ title: "Registro almaceno exitosamente!", icon: "success" }).then(function () { return window.location.href = "Cliente/Grid"; });
+                        }
+                        else {
+                            Toast.fire({ title: data.MsgError, icon: "error" });
+                        }
+                    });
+                }
+                else {
+                    Toast.fire({ title: "Todos los campos son requeridos" });
+                }
+            }
+        },
+        mounted: function () {
+            CreateValidator(this.Formulario);
+        }
+    });
+    Formulario.$mount("#AppEdit");
+})(ClienteEdit || (ClienteEdit = {}));
 //# sourceMappingURL=Edit.js.map
--- Programacion61/wwwroot/App/Cliente/Edit.ts
+++ Programacion61/wwwroot/App/Cliente/Edit.ts
@@ -1,1 +1,43 @@
-﻿
+﻿namespace ClienteEdit {
+
+    var Entity = $("#AppEdit").data('entity');
+
+    var Formulario = new Vue(
+        {
+            data:
+            {
+                Formulario: "#FormEdit",
+                Entity: Entity
+            },
+            methods:
+            {
+                Save() {
+                    if (BValidateData(this.Formulario)) {
+                        Loading.fire("Guardando");
+                        App.AxiosProvider.ClienteGuardar(this.Entity).then(data => {
+                            Loading.close();
+
+                            /*If no error exist*/
+                            if (data.CodeError == 0) {
+
+                                /*Message indicating the file was successfully deleted*/
+                                Toast.fire({ title: "Registro almaceno exitosamente!", icon: "success" }).then(() => window.location.href = "Cliente/Grid")
+                            }
+                            else {
+                                Toast.fire({ title: data.MsgError, icon: "error" })
+                            }
+                        })
+                    }
+                    else {
+                        Toast.fire({ title: "Todos los campos son requeridos" });
+                    }
+                }
+            },
+            mounted() {
+                CreateValidator(this.Formulario)
+            }
+        }
+    );
+
+    Formulario.$mount("#AppEdit")
+}
