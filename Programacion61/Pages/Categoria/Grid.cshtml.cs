c70ea0743a993765f8fb7670f157b2810fe0e4cf
fortiz <fortiz@gbm.net>
2021-12-12 18:48:38 -0600

    Edit/Grid--Categoria

--- Programacion61/Pages/Categoria/Grid.cshtml.cs
+++ Programacion61/Pages/Categoria/Grid.cshtml.cs
@@ -4,13 +4,47 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Entity;
+using WBL;
 
 namespace Programacion61.Pages.Categoria
 {
     public class GridModel : PageModel
     {
-        public void OnGet()
+        private readonly ICategoriaService categoriaService;
+        public IEnumerable<CategoriaEntity> GridList { get; set; } = new List<CategoriaEntity>();
+
+        public GridModel(ICategoriaService categoriaService)
         {
+            this.categoriaService = categoriaService;
+        }
+
+
+        public async Task<IActionResult> OnGet()
+        {
+            try
+            {
+                //Populates the list with the results returned from the customer get method.
+                GridList = await categoriaService.Get();
+                return Page();
+            }
+            catch (Exception ex)
+            {
+                return Content(ex.Message);
+            }
+        }
+
+        public async Task<IActionResult> OnDelete(int id)
+        {
+            try
+            {
+                var result = await categoriaService.Delete(new() { IdCategoria = id });
+                return new JsonResult(result);
+            }
+            catch (Exception ex)
+            {
+                return new JsonResult(new DBEntity { CodeError = ex.HResult, MsgError = ex.Message });
+            }
         }
     }
 }
--- Programacion61/wwwroot/App/Categoria/Grid.js
+++ Programacion61/wwwroot/App/Categoria/Grid.js
@@ -1,2 +1,29 @@
 "use strict";
+var CategoriaGrid;
+(function (CategoriaGrid) {
+    function OnClickDelete(id) {
+        ComfirmAlert("Desea eliminar el registro?", "Eliminar", "warning", "#3085d6", "d33").then(function (result) {
+            if (result.isConfirmed) {
+                /*Message animation to indicate deletion is in progress*/
+                Loading.fire("Eliminacion en proceso");
+                //Call the Axios Service
+                App.AxiosProvider.CategoriaEliminar(id).then(function (data) {
+                    /*Closes the loading animation*/
+                    Loading.close();
+                    /*If no error exist*/
+                    if (data.CodeError == 0) {
+                        /*Message indicating the file was successfully deleted*/
+                        Toast.fire({ title: "Registro eliminado exitosamente!", icon: "success" }).then(function () { return window.location.reload(); });
+                    }
+                    else {
+                        Toast.fire({ title: data.MsgError, icon: "error" });
+                    }
+                });
+            }
+        });
+    }
+    CategoriaGrid.OnClickDelete = OnClickDelete;
+    //Gives HTML Table search, pagination, etc. features.
+    $("#GridView").DataTable();
+})(CategoriaGrid || (CategoriaGrid = {}));
 //# sourceMappingURL=Grid.js.map
