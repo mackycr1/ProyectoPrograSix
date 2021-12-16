844e11377656362c3c712c46c168fcb2e877f124
verny <verny.mata@icumed.com>
2021-12-16 17:01:26 -0600

    Se agrega api para login

---
+++ WebApi/App_Start/ContainerExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DB;
+using Microsoft.Extensions.DependencyInjection;
+using WBL;
+
+namespace WebApi
+{
+    public static class ContainerExtension
+    {
+
+        public static IServiceCollection AddDIContainer(this IServiceCollection services)
+        {
+            services.AddSingleton<IDataAccess, DataAccess>();
+            services.AddTransient<IUsuarioService, UsuarioService>();
+            return services;
+        }
+    }
+}
---
+++ WebApi/Controllers/UsuarioController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WBL;
+using Entity;
+
+namespace WebApiRest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly IUsuarioService UsuarioService;
+
+        public UsuarioController(IUsuarioService usuariosService)
+        {
+            this.UsuarioService = UsuarioService;
+        }
+
+        [HttpPost("Login")]
+        public async Task<UsuarioEntity> Login(UsuarioEntity entity)
+        {
+
+            try
+            {
+                return await UsuarioService.Login(entity);
+            }
+            catch (Exception ex)
+            {
+
+                return new UsuarioEntity() { CodeError=ex.HResult, MsgError=ex.Message };
+            }
+        
+        }
+
+
+
+    }
+}
